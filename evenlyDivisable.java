43fe23521b0f120372e15dfe94ef45c1459853b3
chootingstar <jmirayes96@gmail.com>
2018-12-18 14:38:29 -0800

    main Complete

--- evenlyDivisable.java
+++ evenlyDivisable.java
@@ -1,20 +1,21 @@
 
 public class evenlyDivisable {
 	public static String stuff(String args[]) {
-	int num1 = 14;
+	int num1 = 17;
 			
 	int num2 = 7;
-	public String action;
+	String action;
 	if(num1%num2 == 0) {
-	String action =	"Even Steven";
+	action =	"Even Steven";
 	} else {
-		String action = "Odd Todd";
+		action = "Odd Todd";
 	}
 	
 	return action;
 	
 }
 	public static void main(String[] args) {
-		stuff(args);
+		String bob = stuff(args);
+		System.out.println(bob);
 	}
 }
